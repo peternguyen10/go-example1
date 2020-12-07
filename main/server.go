3c56ffc220ea1228c6a5a8e80bb38cfd881f5a94
peternguyen10 <peter.nguyen1@gmail.com>
2020-12-07 20:22:25 +0000

    c

--- main/server.go
+++ main/server.go
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-package main
-
-import (
-	"fmt"
-	"io"
-	"net/http"
-
-	"sec.demo/hello"
-)
-
-func handle(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, hello.Hello())
-}
-
-func main() {
-	portNumber := "9000"
-	http.HandleFunc("/", handle)
-	fmt.Println("Server listening on port ", portNumber)
-	http.ListenAndServe(":"+portNumber, nil)
-	
-}
-=======
 /*----------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
@@ -52,4 +24,3 @@ func main() {
 	http.ListenAndServe(":"+portNumber, nil)
 
 }
->>>>>>> 0c0c9e9c058a478d78a139fa6e71da51e8e5ee31
